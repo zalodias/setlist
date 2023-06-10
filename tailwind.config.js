4b0410f1e90b437936fceebadc279570c42448d7
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-06-10 17:28:31 +0100

    :recycle: Replace default Tailwind colors with custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      "background-01": "hsl(var(--background-01))",
+      "background-02": "hsl(var(--background-02))",
+      "background-03": "hsl(var(--background-03))",
+      "background-04": "hsl(var(--background-04))",
+      "foreground-01": "hsl(var(--foreground-01))",
+      "foreground-02": "hsl(var(--foreground-02))",
+      "foreground-03": "hsl(var(--foreground-03))",
+      "foreground-04": "hsl(var(--foreground-04))",
     },
   },
-  plugins: [],
-}
+};
